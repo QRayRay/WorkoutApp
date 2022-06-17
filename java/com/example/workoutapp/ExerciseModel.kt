ec50f0abba10e0a79e760b8304753c031d2bbd2f
Raymond C Quintana <77255338+QRayRay@users.noreply.github.com>
2022-06-16 23:24:19 -0500

    Add files via upload

--- java/com/example/workoutapp/ExerciseModel.kt
+++ java/com/example/workoutapp/ExerciseModel.kt
@@ -16,4 +16,35 @@ class ExerciseModel(
     fun setId(id: Int) {
         this.id = id
     }
+
+    fun getName() : String{
+        return name
+    }
+
+    fun setName(id: String) {
+        this.name = name
+    }
+
+    fun getImage() : Int{
+        return image
+    }
+
+    fun setImage(id: Int) {
+        this.image = image
+    }
+
+    fun getIsCompleted() : Boolean{
+        return isCompleted
+    }
+
+    fun setIsCompleted(id: Boolean) {
+        this.isCompleted = isCompleted
+    }
+    fun getIsSelected() : Boolean{
+        return isSelected
+    }
+
+    fun setIsSelected(id: Boolean) {
+        this.isSelected = isSelected
+    }
 }
